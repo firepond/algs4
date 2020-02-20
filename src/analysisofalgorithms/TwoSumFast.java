ee2bed9bfef703fd92778bc2dab2f0c05989938d
firepond <915644848@qq.com>
2020-02-20 14:55:02 +0800

    Add performance logs for 1.4

--- src/analysisofalgorithms/TwoSumFast.java
+++ src/analysisofalgorithms/TwoSumFast.java
@@ -3,27 +3,39 @@ package analysisofalgorithms;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Out;
+import edu.princeton.cs.algs4.BinarySearch;
 
-public class ThreeSum {
+public class TwoSumFast {
 
+    /**
+     * Returns the number of triples (i, j, k) with {@code i < j < k} such that
+     * {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     * @return the number of triples (i, j, k) with {@code i < j < k} such that
+     *         {@code a[i] + a[j] + a[k] == 0}
+     */
     public static int count(int[] a) {
         int n = a.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        count++;
-                    }
-                }
+            if (BinarySearch.indexOf(a, -a[i]) > i) {
+                count++;
             }
         }
         return count;
     }
 
+    /**
+     * Reads in a sequence of integers from a file, specified as a command-line
+     * argument; counts the number of triples sum to exactly zero; prints out the
+     * time to perform the computation.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         String dataPath = "./algs4-data/";
-        Out log = new Out("./log/ThreeSum.txt");
+        Out log = new Out("./log/TwoSumFast.txt");
         for (int i = 1; i <= 32; i *= 2) {
             String fileName = i + "Kints.txt";
             In in = new In(dataPath + fileName);
@@ -32,7 +44,7 @@ public class ThreeSum {
             StdOut.println(fileName);
             Stopwatch timer = new Stopwatch();
             int count = count(a);
-            var time = timer.elapsedTime();
+            var time= timer.elapsedTime();
             StdOut.println("elapsed time = " + time);
             log.println(time);
             StdOut.println(count);
