40a682d5079e9f52d494ec45c2f531a4f6337c9d
firepond <915644848@qq.com>
2020-02-27 10:31:11 +0800

    Update 1.3.45

--- src/stacksandqueues/StackGenerability.java
+++ src/stacksandqueues/StackGenerability.java
@@ -3,8 +3,7 @@ package stacksandqueues;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * @author firepond 
- * algs4 exercise 1.3.45
+ * @author firepond algs4 exercise 1.3.45
  */
 
 public class StackGenerability {
@@ -12,7 +11,7 @@ public class StackGenerability {
     private static boolean willTheStackUnderflow(String[] inputValues) {
         int itemsCount = 0;
 
-        for(String input : inputValues) {
+        for (String input : inputValues) {
             if (input.equals("-")) {
                 itemsCount--;
             } else {
@@ -27,8 +26,29 @@ public class StackGenerability {
         return false;
     }
 
-    
+    private static boolean canAPermutationBeGenerated(String[] permutation) {
+        Stack<Integer> stack = new Stack<>();
+        int current = 0;
 
+        for (String value : permutation) {
+            int integerValue = Integer.parseInt(value);
+
+            if (stack.isEmpty() || integerValue > stack.peek()) {
+                while (current < integerValue) {
+                    stack.push(current);
+                    current++;
+                }
+
+                current++;
+            } else if (integerValue == stack.peek()) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
         StdOut.println("Exercises makes perfect");
