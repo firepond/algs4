0f348560a311d7360d5d858d10b839ee3be7181f
firepond <915644848@qq.com>
2020-08-08 16:08:31 +0800

    Update exercises in 2.1.

--- src/sorting/Exercise10.md
+++ src/sorting/Exercise10.md
@@ -1,2 +1,3 @@
 ### ALGS4 Exercise 2.1.10
 
+Already partly sorted.
--- src/sorting/Exercise11.java
+++ src/sorting/Exercise11.java
@@ -1,28 +1,35 @@
 package sorting;
 
-import edu.princeton.cs.algs4.*;
+import java.util.ArrayList;
 
-public class Shell extends Sort {
+//Shell sort which sort store h in an array instead of calculating it in real-time.
+public class Exercise11 extends Sort {
 
     @SuppressWarnings("rawtypes")
     public static void sort(Comparable[] a) {
         int N = a.length;
         int h = 1;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
         while (h < N / 3) {
             h = 3 * h + 1;
+            list.add(h);
         }
-        while (h >= 1) {
+        Integer[] hs = new Integer[list.size()];
+        hs = list.toArray(hs);
+        int hIndex = hs.length - 1;
+        while (hIndex >= 0) {
+            h = hs[hIndex];
             for (int i = h; i < N; i++) {
                 for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
                     exch(a, j, j - h);
                 }
             }
-            h /= 3;
+            hIndex--;
         }
     }
 
     public static void main(String[] args) {
-        In in = new In("./algs4-data/tiny.txt");
         String s = "E A S Y S H E L L S O R T Q U E S T I O N";
         String[] a = s.split(" ");
         sort(a);
