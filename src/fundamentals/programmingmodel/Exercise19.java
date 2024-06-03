12570a4bc6203da2b0a73129034161a85cbc06c4
firepond <915644848@qq.com>
2024-06-03 15:48:00 +0800

    Add BigInteger.

--- src/fundamentals/programmingmodel/Exercise19.java
+++ src/fundamentals/programmingmodel/Exercise19.java
@@ -1,5 +1,7 @@
 package fundamentals.programmingmodel;
 
+import java.math.BigInteger;
+
 /**
  * @author firepond
  * algs4 exercise 1.1.19
@@ -10,8 +12,10 @@ import edu.princeton.cs.algs4.*;
 public class Exercise19 {
 
     public static void main(String[] args) {
-        for (int N = 0; N < 80; N++) {
-            StdOut.println(N + " " + F2(N));
+        int N = 10000;
+        BigInteger[] fArray = FBigArray(N);
+        for (int i = 0; i < N; i++) {
+            StdOut.println(i + " " + fArray[i]);
         }
     }
 
@@ -25,15 +29,26 @@ public class Exercise19 {
         return F(N - 1) + F(N - 2);
     }
 
-    public static long F2(int N) {
-        long left = 1;
-        long right = 1;
-        long temp;
-        for (int i = 2; i < N; i++) {
-            temp = right;
-            right = left + right;
-            left = temp;
+    public static BigInteger FBig(BigInteger N) {
+        if (N.equals(BigInteger.ZERO)) {
+            return BigInteger.ZERO;
         }
-        return right;
+        if (N.equals(BigInteger.ONE)) {
+            return BigInteger.ONE;
+        }
+        BigInteger b1 = N.subtract(BigInteger.ONE);
+        BigInteger b2 = N.subtract(BigInteger.TWO);
+        BigInteger result = FBig(b1).add(FBig(b2));
+        return result;
+    }
+
+    public static BigInteger[] FBigArray(int N) {
+        BigInteger[] array = new BigInteger[N];
+        array[0] = BigInteger.ZERO;
+        array[1] = BigInteger.ONE;
+        for (int i = 2; i < N; i++) {
+            array[i] = array[i - 1].add(array[i - 2]);
+        }
+        return array;
     }
 }
